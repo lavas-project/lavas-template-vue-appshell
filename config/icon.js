127bdb801c94ac76388da552609bf5d69f518f0b
zoumiaojiang <zoumiaojiang@gmail.com>
2017-06-20 19:07:01 +0800

    fecs

--- config/icon.js
+++ config/icon.js
@@ -1,10 +1,16 @@
+/**
+ * @file icon 构建相关配置
+ * @author *__ author __*{% if: *__ email __* %}(*__ email __*){% /if %}
+ */
+
 var path = require('path');
 
 module.exports = {
     // 前缀
     prefix: 'svg-',
     // 用户自定义的svg文件夹
-    svgDir: path.resolve(__dirname, '../src/assets/svg'),
+    svgDir: path.resolve(__dirname, '../src/assets/svg')
+    // ,
     // 项目中使用的fontawesome名
     // icons: [
     //     'envelope'
--- src/entry-client.js
+++ src/entry-client.js
@@ -7,7 +7,7 @@ import Vue from 'vue';
 import 'es6-promise/auto';
 import FastClick from 'fastclick';
 import {createApp} from './app';
-import ProgressBar from '@/components/ProgressBar.vue'
+import ProgressBar from '@/components/ProgressBar.vue';
 
 import 'normalize.css';
 import '@/assets/styles/global.styl';
@@ -22,8 +22,8 @@ const {app, router, store} = createApp();
 
 // a global mixin that calls `asyncData` when a route component's params change
 Vue.mixin({
-    beforeRouteUpdate (to, from, next) {
-        const {asyncData} = this.$options;
+    beforeRouteUpdate(to, from, next) {
+        const asyncData = this.$options.asyncData;
         if (asyncData) {
             asyncData({
                 store: this.$store,
@@ -43,8 +43,9 @@ router.beforeResolve((to, from, next) => {
 
     let diffed = false;
     const activated = matched.filter((c, i) => {
-        return diffed || (diffed = (prevMatched[i] !== c));
-    })
+        const ret = diffed || (diffed = (prevMatched[i] !== c));
+        return ret;
+    });
 
     if (!activated.length) {
         return next();
@@ -53,10 +54,13 @@ router.beforeResolve((to, from, next) => {
     loading.start();
     Promise.all(activated.map(c => {
         if (c.asyncData && (!c.asyncDataFetched || to.meta.notKeepAlive)) {
-            return c.asyncData({ store, route: to })
-                .then(() => {
-                    c.asyncDataFetched = true;
-                });
+            return c.asyncData({
+                store,
+                route: to
+            })
+            .then(() => {
+                c.asyncDataFetched = true;
+            });
         }
     })).then(() => {
         loading.finish();
