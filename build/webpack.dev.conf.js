12f8d558aa80495c0f99b9191c785d0e60313f1d
huanghuiquan <huanghuiquan@baidu.com>
2017-07-06 17:35:29 +0800

    fix skeleton hot reload

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -57,6 +57,7 @@ module.exports = merge(baseWebpackConfig, {
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: 'index.html',
+            cache: false,
             inject: true,
             favicon: utils.assetsPath('img/icons/favicon.ico')
         }),
