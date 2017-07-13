49306a766765217022ed192743276bd1e30b91de
xiaoiver <pyqiverson@gmail.com>
2017-07-13 23:06:31 +0800

    更新vue-skeleton插件版本

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -29,8 +29,11 @@ module.exports = merge(baseWebpackConfig, {
     module: {
         rules: utils.styleLoaders({sourceMap: config.dev.cssSourceMap})
             .concat(SkeletonWebpackPlugin.loader({ // visit by route '/skeleton' in dev mode
-                entry: 'skeleton',
-                routerEntry: resolve('src/router.js')
+                resource: resolve('src/router.js'),
+                options: {
+                    entry: 'skeleton',
+                    routePathTemplate: '/skeleton'
+                }
             }))
     },
 
